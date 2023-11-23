6c2ad615e6c4f50c4217aaad7dddf27f9fed6e2a
ImChri2 <chris@chris-industries.de>
2023-11-23 12:10:53 +0100

    added funktion to buttons on Übung07

--- IAS/Übung/Übung07/TicTacToe/src/games/exapmles/tictactoe/Gameoption.java
+++ IAS/Übung/Übung07/TicTacToe/src/games/exapmles/tictactoe/Gameoption.java
@@ -4,6 +4,8 @@ import games.basic.gameObjects.interfaces.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Gameoption extends JPanel {
     private JButton button1;
@@ -14,6 +16,12 @@ public class Gameoption extends JPanel {
         button1 = new JButton("New Game");
         button2 = new JButton("Reset");
 
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
         // Füge die Buttons zum Panel hinzu
         add(button1);
         add(button2);
