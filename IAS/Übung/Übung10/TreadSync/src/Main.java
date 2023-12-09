2521ca11b766303a02cf51719212100b46f4c933
Chris <chris@chris-industries.de>
2023-12-09 14:50:25 +0100

    changed Übung10 Threadsycn works now

--- IAS/Übung/Übung10/TreadSync/src/Main.java
+++ IAS/Übung/Übung10/TreadSync/src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 1;
-        Thread1 thread1 = new Thread1(i);
-        Thread1 thread2 = new Thread1(i);
+        Thread1 thread1 = new Thread1();
+        Thread1 thread2 = new Thread1();
 
         thread1.start();
         thread2.start();
--- IAS/Übung/Übung10/TreadSync/src/Thread2.java
+++ IAS/Übung/Übung10/TreadSync/src/Thread2.java
@@ -1,22 +1,19 @@
 import java.util.concurrent.TimeUnit;
 
 class Thread1 extends Thread {
-    private int i;
-
-    public Thread1(int i) {
-        this.i = i;
-    }
+    private static int i = 1;
 
     @Override
-    public synchronized void run() {
+    public void run() {
         try {
-            // Hier können deine Thread-Aktionen stattfinden
             TimeUnit.SECONDS.sleep(1);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        i++;
-        System.out.println(i);
+        synchronized (Thread1.class) {
+            i++;
+            System.out.println(i);
+        }
     }
 }
