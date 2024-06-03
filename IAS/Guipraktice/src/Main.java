11b58c7e5b1a5742c7e5f75caa35b3fde0ba023e
ImChri2 <chris@chris-industries.de>
2024-06-03 20:43:34 +0200

    added ias übungen

--- IAS/Guipraktice/src/Main.java
+++ IAS/Guipraktice/src/Main.java
@@ -7,34 +7,39 @@ import java.util.concurrent.Flow;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Test Formular");
-        JPanel pheading = new JPanel();
-        pheading.setLayout(new BoxLayout(pheading, BoxLayout.X_AXIS));
+        frame.setLayout(new BorderLayout());
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
+        panel.setLayout(new GridLayout(4, 2, 10, 10));
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel heading = new JLabel("Formula");
+        //JLabel heading = new JLabel("Formula");
         JLabel nameLabel = new JLabel("Enter your name:");
-        JTextField nameField = new JTextField(20);
+        JTextField nameField = new JTextField(1);
+        JLabel LivingLabel = new JLabel("Wohnort:");
+        JTextField LivingField = new JTextField(1);
+
         JLabel greetingLabel = new JLabel("");
+        JButton Submit = new JButton("Submit");
 
         JLabel dateLabel = new JLabel("Select a date:");
         JSpinner dateSpinner = new JSpinner(new SpinnerDateModel());
         JSpinner.DateEditor dateEditor = new JSpinner.DateEditor(dateSpinner, "yyyy-MM-dd");
         dateSpinner.setEditor(dateEditor);
 
-        frame.add(pheading);
-        frame.add(panel);
-        pheading.add(heading);
+        frame.add(BorderLayout.CENTER, panel);
         panel.add(nameLabel);
         panel.add(nameField);
-        panel.add(greetingLabel);
         panel.add(dateLabel);
-        panel.add(dateSpinner);
+        //panel.add(dateSpinner);
         panel.add(dateEditor);
+        panel.add(LivingLabel);
+        panel.add(LivingField);
+
+        frame.add(BorderLayout.SOUTH, Submit);
 
         panel.setVisible(true);
-        pheading.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        //pheading.setVisible(true);
         frame.setVisible(true);
     }
 }
